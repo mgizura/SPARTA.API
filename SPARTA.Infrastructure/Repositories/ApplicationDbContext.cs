b27cc907eb82290e914ba0be05a47cfeead0663a
mizura <mizura@clarika.com.ar>
2025-11-26 15:44:46 -0300

    fix init

--- SPARTA.Infrastructure/Repositories/ApplicationDbContext.cs
+++ SPARTA.Infrastructure/Repositories/ApplicationDbContext.cs
@@ -12,8 +12,9 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        
+
         // Aquí se configurarán las entidades cuando se creen
     }
 }
+
 
